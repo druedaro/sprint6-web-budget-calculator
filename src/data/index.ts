6f034629b9827ec120da5583fd8d48d7078d1d99
David <ruedarosasdavid@gmail.com>
2025-09-30 22:35:54 +0200

    feat: add basic services data

---
+++ src/data/index.ts
@@ -0,0 +1,22 @@
+import type { Service } from '../types/';
+
+export const SERVICES_DATA: Service[] = [
+  {
+    id: 'seo',
+    name: 'SEO Campaign',
+    price: 300,
+    selected: false,
+  },
+  {
+    id: 'ads',
+    name: 'Google Ads Campaign',
+    price: 400,
+    selected: false,
+  },
+  {
+    id: 'web',
+    name: 'Web Development',
+    price: 500,
+    selected: false,
+  },
+];
